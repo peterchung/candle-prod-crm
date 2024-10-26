3bc8b0ab8f97fad8394b26b9721f0c8ef0287702
peter chung <peter.chung.716@gmail.com>
2024-10-25 23:02:12 -0400

    chore: initialize candle prod crm

---
+++ src/routes/monday.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+const router = Router();
+
+import * as transformationController from '../controllers/monday-controller';
+import authenticationMiddleware from '../middlewares/authentication';
+
+router.post(
+  '/api/monday/execute_action',
+  authenticationMiddleware,
+  transformationController.executeAction
+);
+
+export default router;
