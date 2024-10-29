82fe63f2120c837fc04343d2ed3d06dc1b7383c9
peter chung <peter.chung.716@gmail.com>
2024-10-28 23:34:01 -0400

    feat: add order form with TextField, Dropdown and Accordion components

--- client/src/board-views/OrderForm.jsx
+++ client/src/board-views/OrderForm.jsx
@@ -4,7 +4,15 @@ import '../App.css';
 import mondaySdk from 'monday-sdk-js';
 // import 'monday-ui-react-core/dist/main.css';
 import 'monday-ui-style/dist/index.min.css';
-import { Dropdown } from 'monday-ui-react-core';
+import {
+  Dropdown,
+  TextField,
+  Accordion,
+  AccordionItem,
+  Flex,
+  Box,
+  Button,
+} from 'monday-ui-react-core';
 import axios from 'axios';
 
 const monday = mondaySdk();
@@ -66,6 +74,15 @@ const OrderForm = () => {
   const [searchValue, setSearchValue] = useState('');
   const [context, setContext] = useState();
   const [allFragrances, setAllFragrances] = useState([]);
+  const [selectedFragrances, setSelectedFragrances] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [candleInscription, setCandleInscription] = useState('');
+  const [boxQuantity, setBoxQuantity] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
 
   useEffect(() => {
     monday.listen('context', (res) => {
@@ -75,6 +92,8 @@ const OrderForm = () => {
     getFragranceAndSetOptions(setAllFragrances);
   }, []);
 
+  console.log('fragrances', allFragrances);
+
   const allOptions = useMemo(() => allFragrances, [allFragrances]);
 
   const options = useMemo(() => {
@@ -83,18 +102,153 @@ const OrderForm = () => {
       option.label.toLowerCase().includes(searchValue.toLowerCase())
     );
   }, [allOptions, searchValue]);
+
   const onInputChange = (value) => {
     setSearchValue(value);
   };
 
+  const onChange = (value) => {
+    setSelectedFragrances((prevSelected) => [...prevSelected, value]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      firstName,
+      lastName,
+      email,
+      phone,
+      candleInscription,
+      boxQuantity,
+      selectedFragrances,
+      city,
+      state,
+    };
+
+    console.log('Form Data:', formData);
+  };
+
   return (
-    <Dropdown
-      options={options}
-      multi
-      placeholder='Select fragrances'
-      className='dropdown-stories-styles_with-chips'
-      onInputChange={onInputChange}
-    />
+    <form onSubmit={handleSubmit}>
+      <Flex
+        direction={Flex.directions.COLUMN}
+        gap={Flex.gaps.MEDIUM}
+        align={Flex.align.CENTER}
+      >
+        <div style={{ width: '800px' }}>
+          <Box marginBottom={Box.marginBottoms.LARGE}>
+            <Accordion
+              defaultIndex={[2]}
+              allowMultiple
+              style={{ width: '500px' }}
+            >
+              <AccordionItem title='1. Customer Contact Information'>
+                <Flex
+                  direction={Flex.directions.COLUMN}
+                  gap={Flex.gaps.MEDIUM}
+                  align={Flex.align.CENTER}
+                >
+                  <div style={{ width: '700px' }}>
+                    <TextField
+                      placeholder='John'
+                      title='First Name'
+                      required
+                      value={firstName}
+                      onChange={(value) => setFirstName(value)}
+                    />
+                    <TextField
+                      placeholder='Doe'
+                      title='Last Name'
+                      required
+                      value={lastName}
+                      onChange={(value) => setLastName(value)}
+                    />
+                    <TextField
+                      placeholder='email@monday.com'
+                      title='Email'
+                      required
+                      type={TextField.types.EMAIL}
+                      value={email}
+                      onChange={(value) => setEmail(value)}
+                    />
+                    <TextField
+                      placeholder='Digits only (e.g.1234567890)'
+                      title='Phone'
+                      required
+                      type={TextField.types.TEL}
+                      value={phone}
+                      onChange={(value) => setPhone(value)}
+                    />
+                  </div>
+                </Flex>
+              </AccordionItem>
+              <AccordionItem title='2. Shipping Address'>
+                <Flex
+                  direction={Flex.directions.COLUMN}
+                  gap={Flex.gaps.MEDIUM}
+                  align={Flex.align.CENTER}
+                >
+                  <div style={{ width: '700px' }}>
+                    <TextField
+                      placeholder='New York'
+                      title='City'
+                      required
+                      value={city}
+                      onChange={(value) => setCity(value)}
+                    />
+                    <TextField
+                      placeholder='NY'
+                      title='State'
+                      required
+                      value={state}
+                      onChange={(value) => setState(value)}
+                    />
+                  </div>
+                </Flex>
+              </AccordionItem>
+            </Accordion>
+          </Box>
+        </div>
+        <div style={{ width: '800px' }}>
+          {/* <h5>Candle inscription</h5> */}
+          <TextField
+            placeholder='For my best friend forever'
+            title='Candle inscription'
+            size={TextField.sizes.MEDIUM}
+            value={candleInscription}
+            onChange={(value) => setCandleInscription(value)}
+          />
+          {/* <h5>Quantity (box)</h5> */}
+          <TextField
+            placeholder='Enter box quantity'
+            title='Quantity (box)'
+            required={true}
+            requiredAsterisk={true}
+            size={TextField.sizes.MEDIUM}
+            type={TextField.types.NUMBER}
+            style={{ fontWeight: 'bold' }}
+            value={boxQuantity}
+            onChange={(value) => setBoxQuantity(value)}
+          />
+          {/* <Box marginTop={Box.marginYs.MEDIUM}> */}
+          {/* <h5>Fragrances</h5> */}
+          <Dropdown
+            options={options}
+            multi
+            multiline
+            placeholder='Select fragrances'
+            className='dropdown-stories-styles_with-chips'
+            onInputChange={onInputChange}
+            onChange={onChange}
+            style={{ width: '300px' }}
+          />
+          {/* </Box> */}
+        </div>
+        <Box marginBottom={Box.marginBottoms.XXL}>
+          <Button type={Button.types.SUBMIT}>Submit</Button>
+        </Box>
+      </Flex>
+    </form>
   );
 };
 
